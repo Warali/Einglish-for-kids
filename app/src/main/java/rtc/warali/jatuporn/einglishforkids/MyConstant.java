df347c645d597e783fde6a1ee962a9ebfa3f04da
warali <chakuza_555@hotmail.com>
2017-01-05 22:35:54 +0700

    Start jan

--- app/src/main/java/rtc/warali/jatuporn/einglishforkids/MyConstant.java
+++ app/src/main/java/rtc/warali/jatuporn/einglishforkids/MyConstant.java
@@ -15,6 +15,7 @@ public class MyConstant {
             "Tiger","Unicorn", "Violin","Whale",
             "Xylophone","Yak","Zeber"};
 
+
     private int[] trueAnserInts = new int[]{2,1,3,1,2,
                                             1,1,1,2,3,
                                             1,3,1,2,1,
